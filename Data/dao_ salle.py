a3eb502684e0d548e1341f1ff77e7a4c24f9cb01
Yanis Saaoui <saaouiyanis132@gmail.com>
2026-04-19 21:42:18 -0400

    Ajout insert_salle

--- Data/dao_ salle.py
+++ Data/dao_ salle.py
@@ -3,3 +3,13 @@ def get_connection(self) :
         config=json.load(f)
 
     return mysql.connector.connect(**config)
+
+def insert_salle(self, salle):
+    conn= self.get_connection()
+    cursor=conn.cursor()
+    cursor.execute(
+        "INSERT INTO salle VALUES (%s,%s,%s,%s)",
+        (salle.code, salle.description, salle.categorie, salle.capacite)
+    )
+    conn.commit()
+    conn.close()
