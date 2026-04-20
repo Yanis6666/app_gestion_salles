3a2a49095d276406a2811e733b9434d93e91bffd
Yanis Saaoui <saaouiyanis132@gmail.com>
2026-04-19 22:24:52 -0400

    Test  Service

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from models.salle import Salle
 dao=DataSalle()
 dao.insert_salle(Salle("INF1", "Test","Classe", 40))
 print(dao.get_salles())
+
+
+from services.services_salle import  ServiceSalle
+from models.salle import Salle
+
+s=ServiceSalle()
+print(s.ajouter_salle(Salle("INF2","Info","Lab",35)))
