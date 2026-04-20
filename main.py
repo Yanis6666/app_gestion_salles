1c541b64eea8e951cda1f21958ff36f6dca34e3f
Yanis Saaoui <saaouiyanis132@gmail.com>
2026-04-19 21:51:27 -0400

    Test DAO

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Data.dao_salle import DataSalle
 from models.salle import Salle
 
-dao =DataSalle()
-s= Salle("INF1", "Salle test", "Classe", 40)
-dao.insert_salle(s)
-
+dao=DataSalle()
+dao.insert_salle(Salle("INF1", "Test","Classe", 40))
 print(dao.get_salles())
