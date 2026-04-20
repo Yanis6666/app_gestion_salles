a8a7e261b08da5b9337f69108ec08ea4155bfa8e
Yanis Saaoui <saaouiyanis132@gmail.com>
2026-04-19 21:39:14 -0400

    Ajout config DB

--- Data/dao_ salle.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-import json
-
-from models.salle import Salle
-class DataSalle :
-    def get_connection(self) :
-        with open("Data/config.json") as f:
-            config=json.load(f)
-        return mysql
---
+++ services/service_salle.py
@@ -0,0 +1,31 @@
+from Data.dao_salle import DataSalle
+
+class ServiceSalle:
+
+    def __init__(self):
+        self.dao = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie:
+            return False, "Champs obligatoires manquants"
+
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+
+        self.dao.insert_salle(salle)
+        return True, "Salle ajoutée"
+
+    def modifier_salle(self, salle):
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+        self.dao.update_salle(salle)
+        return True, "Salle modifiée"
+
+    def supprimer_salle(self, code):
+        self.dao.delete_salle(code)
+
+    def rechercher_salle(self, code):
+        return self.dao.get_salle(code)
+
+    def recuperer_salles(self):
+        return self.dao.get_salles()
